9178e9b8d2e81f897ba0db8a0339de854c2608b4
Lambrous Empemano <lambrous777@gmail.com>
2022-03-01 18:16:05 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
     hot: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
